bacbdc4e892678c549ee40e02cd7ca9967351b07
이준용 <your_email@example.com>
2026-09-08 16:52:52 +0900

    refactor: set up programmatic UI

---
+++ RxChat/Presentation/Scene/Auth/Login/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  RxChat
+//
+//  Created by 이준용 on 9/8/26.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    // MARK: - Properties
+
+    // MARK: - ViewModel
+
+    // MARK: - UI Components
+
+    // MARK: - Initializer
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - UI Configurations
+
+    // MARK: - Bindings
+
+    // MARK: - Private Methods
+
+
+}
