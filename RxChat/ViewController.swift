eef09784d05364ae4b300fee2995fed6c2b3d92d
이준용 <your_email@example.com>
2026-09-08 16:38:13 +0900

    chore: initialize RxChat iOS project

---
+++ RxChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxChat
+//
+//  Created by 이준용 on 9/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
